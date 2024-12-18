ef7d9f3439d1a0741fee4b8994ea113021d113ae
xiaoCRQ <xiaocrq001@gmail.com>
2024-12-18 10:57:12 +0800

    normal

--- js/index.js
+++ js/index.js
@@ -190,8 +190,8 @@ async function OpeWebSite() {
 
   // 宽度动画
   const widthAnimation = gsap.to(Progress, {
-    width: '110vw',  // 结束宽度
-    duration: 5,  // 动画持续时间
+    width: '90vw',  // 结束宽度
+    duration: 7.5,  // 动画持续时间
     ease: "expo.inOut",  // 缓动效果
     paused: false   // 初始时不暂停
   });
